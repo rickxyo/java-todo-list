c305a4787bf22aa1da7f76b94ced5f97d123e818
rickxyo <pedrinhooldoni@gmail.com>
2025-06-07 22:23:57 -0300

    add: user interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Welcome to your To Do List!");
+            System.out.println("1 - Add task");
+            System.out.println("2 - Remove task");
+            System.out.println("3 - Edit task");
+            System.out.println("4 - Show all tasks");
+            System.out.println("5 - Exit");
+            int number = scanner.nextInt();
+        }
+
     }
 }
